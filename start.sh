561cbe0deb6f849a5c5e4ee44f7720a4d3170052
Jack McNicol <jackmcpickle@gmail.com>
2015-02-18 15:59:32 +1030

    clean up start script and add back ENV replace

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Disable Strict Host checking for non interactive git clones
-
-echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
 
 # Pull down code form git for our site!
 if [ ! -z "$GIT_REPO" ]; then
@@ -19,6 +16,24 @@ fi
 
 procs=$(cat /proc/cpuinfo |grep processor | wc -l)
 sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
+
+# Very dirty hack to replace variables in code with ENVIRONMENT values
+if [ ! -z "$ENV_REPLACE_FOLDER" ]; then
+   folder=$ENV_REPLACE_FOLDER
+else
+   folder=/usr/cms/public
+fi
+
+echo "Replacing all env vars in ${folder}"
+
+for i in $(env)
+do
+  variable=$(echo "$i" | cut -d'=' -f1)
+  value=$(echo "$i" | cut -d'=' -f2)
+  if [[ "$variable" != '%s' ]] ; then
+    replace='\$\$_'${variable}'_\$\$'
+    find $folder -type f -exec sed -i -e 's@'${replace}'@'${value}'@g' {} \; ; fi
+  done
 
 # Start supervisord and services
 /usr/local/bin/supervisord -n
